05ebacbf03c965cb072cc01d5ecaa7ff40f0117d
oksi18 <oksanaklymchuk04@gmail.com>
2023-10-24 18:52:22 +0300

    mail

--- src/main/java/com/example/sep2022javaspring/services/MailService.java
+++ src/main/java/com/example/sep2022javaspring/services/MailService.java
@@ -14,7 +14,7 @@ public class MailService {
     public void notifyCreatedCar(CarDto createdCar){
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(System.getProperty("spring.mail.username"));
-        message.setTo("demchyshyn.v87@gmail.com");
+        message.setTo("oksanaklymchuk04@gmail.com");
         message.setSubject("New Car");
         message.setText("A new car was created id: %d, model: %s, producer: %s, power: %d".formatted(createdCar.getId(), createdCar.getModel(), createdCar.getProducer(), createdCar.getPower()));
         mailSender.send(message);
@@ -23,7 +23,7 @@ public class MailService {
     public void notifyDeletedCar(int id){
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(System.getProperty("spring.mail.username"));
-        message.setTo("demchyshyn.v87@gmail.com");
+        message.setTo("oksanaklymchuk04@gmail.com");
         message.setSubject("deleted Car");
         message.setText("car with id: %d was deleted".formatted(id));
         mailSender.send(message);
